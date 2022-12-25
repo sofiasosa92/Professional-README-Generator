be8b2d9f5c21d288c0e9ca9b5b3e16d823453e48
Sofia92 <sofiasosaperez@outlook.com>
2022-12-24 20:36:36 -0600

    index.js

--- develop/generateMarkdown.js
+++ develop/generateMarkdown.js
@@ -1,34 +1,27 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  switch (license) {
-    case "Apache License 2.0":
-      return "![Npm package license](https://badgen.net/npm/license/discord.js)";
-    case "GNU General Public License v3.0":
-      return "https://img.shields.io/badge/license-GNU%20v3.0-blue";
-    case "MIT License":
-      return "![PyPi license](https://badgen.net/pypi/license/pip/)";
-    case "BSD 2-Clause Simplified License":
-      return "![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg) ";
-    case "BSD 3-Clause New or Revised License":
-      return "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
-  }
-}
-module.exports = renderLicenseBadge;
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {
+//   switch (license) {
+//     case "Apache License 2.0":
+//       return "![Npm package license](https://badgen.net/npm/license/discord.js)";
+//     case "GNU General Public License v3.0":
+//       return "https://img.shields.io/badge/license-GNU%20v3.0-blue";
+//     case "MIT License":
+//       return "![PyPi license](https://badgen.net/pypi/license/pip/)";
+//     case "BSD 2-Clause Simplified License":
+//       return "![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg) ";
+//     case "BSD 3-Clause New or Revised License":
+//       return "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+//   }
+// }
+// module.exports = renderLicenseBadge;
 
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) { }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) { }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-`;
-}
-
-module.exports = generateMarkdown;
--- develop/index.js
+++ develop/index.js
@@ -2,14 +2,18 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 //terminal messages
 console.log("Welcome to my README generator")
 console.log("Answwer the following questions to generate a high quality README for your project")
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () =>
+inquirer.prompt ([
     {
         type: "input",
         name: "name",
@@ -47,11 +51,11 @@ const questions = [
         name: "license",
         message: "What license are you using?",
         choices: [
-            "Apache License 2.0",
-            "GNU General Public License v3.0",
-            "MIT License",
-            "BSD 2-Clause Simplified License",
-            "BSD 3-Clause New or Revised License",
+            "Apache 2.0",
+            "GNU General Public v3.0",
+            "MIT",
+            "BSD 3",
+            "None"
         ],
     },
     {
@@ -64,28 +68,59 @@ const questions = [
         name: "email",
         message: "What is your E-mail?",
     },
-];
+]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        
-        console.log("Success! You can now preview your README file");
-    });
-};
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then(function (userInput) {
-        console.log(userInput)
-        writeToFile("README.md",generateMarkdown(userInput));
-   
-    });
-};
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
 
-// Function call to initialize app
-init();
+    let badge = "";
+    if(data.license == "Apache 2.0"){
+        badge = "![Npm package license](https://badgen.net/npm/license/discord.js)"
+    }else if (data.license == "GNU General Public v3.0") {
+        badge = "https://img.shields.io/badge/license-GNU%20v3.0-blue"
+    }else if (data.license == "MIT") {
+        badge = "![PyPi license](https://badgen.net/pypi/license/pip/)"
+    }else if (data.license == "BSD 3") {
+        badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+    }
+
+    
+    return`# ${data.title}  
+    ${badge}
+    ${data.description}
+    ## Table of Contents:
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    ### Installation:
+    In order to install the necessary dependencies, open the console and run the following:
+    \`\`\`${data.installations}\`\`\`
+    ### Usage:
+    ${data.usage}
+    ### License:
+    This project is licensed under:
+    ${data.license}
+    ### Contributing:
+    ${data.contribute}
+    ### Tests:
+    In order to test open the console and run the following:
+    \`\`\`${data.tests}\`\`\`
+    ### Questions:
+    If you have any questions contact me on [GitHub](https://github.com/${data.username}) or contact 
+    ${data.author} at ${data.email}  
+     `
+    }
+
+  // TODO: Create a function to write README file
+    questions()
+    .then((data) =>writeFileAsync('generatedREADME.md', generateMarkdown(data)))
+    .then(() => console.log("Success! You can now preview your README file"))
+    .catch((err) => console.log(err));
+
+
+
+// // TODO: Create a function to initialize app
